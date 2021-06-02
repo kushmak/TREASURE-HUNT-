82a221cf4b8609b5e83bd9574b9d95f3f994c4a9
KUSH MEHROTRA <78347631+kushmak@users.noreply.github.com>
2021-06-02 20:38:03 +0530

    Add files via upload

---
+++ js/System.js
@@ -0,0 +1,15 @@
+class System{
+
+    constructor(){}
+
+    // Add code to authenticate the given code and the access codes.
+    authenticate(actualCode,enterdedCode){
+        if(actualCode===enterdedCode)
+        return  true
+        else
+        return false
+        
+        }
+    
+
+}
